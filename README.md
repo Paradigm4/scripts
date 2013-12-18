0affdd81616b5343bd03137c9af96763afe55828
Bryan Lewis <blewis@arbitrage.(none)>
2013-12-17 22:07:52 -0500

    Parallel save/load

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 scripts
 =======
 
-Miscellaneous SciDB scripts and utilities
+## Miscellaneous SciDB scripts and utilities
 
-* scidb_backup.sh is a basic script that backs up SciDB databases to files and reloads them.
+### scidb_backup.sh
+
+This is a basic script that backs up SciDB databases to files and reloads them.
+The script helps automate some of the details by saving all arrays listed in
+the database along with a manifest of the saved arrays and their schema.
+
+It can save arrays in SciDB's 'opaque' storage format, or in binary format
+based on the array attribute types as a fallback if the opaque storage format
+won't work (for example across incompatible SciDB versions). Beware that the
+binary storage format only saves the current version of the listed arrays.
+
+Examples follow:
+
+* Back up all data to one directory named 'backupdir' on the SciDB coordinator instance
+using the SciDB 'opaque' storage format:
+```
+  scidb_backup save-opaque backupdir
+```
+* ...and the reload the data...
+```
+  scidb_backup restore-opaque backupdir
+```
+* Back up all data to one directory named 'backupdir' on the SciDB coordinator instance
+using binary storage formats based on array attributes (also showing reload):
+```
+  scidb_backup save-binary backupdir
+  scidb_backup restore-binary backupdir
+```
+* Back up and restore all data in parallel to one subdirectory per SciDB instance, located inside each instance data directory, using the SciDB 'opaque' storage format:
+```
+  scidb_backup save-opaque backupdir 1
+  scidb_backup restore-opaque backupdir 1
+```
--- scidb_backup.sh
+++ scidb_backup.sh
@@ -6,63 +6,111 @@
 
 unalias iquery >/dev/null 2>&1
 read -d '' usage << "eof"
+
 Save SciDB data with:
-./scidb_backup.sh save-opaque <backup directory to save data to>
--or-
-./scidb_backup.sh save-binary <backup directory to save data to>
+./scidb_backup.sh save-opaque <directory to save data to> [parallel]
+  -or-
+./scidb_backup.sh save-binary <directory to save data to> [parallel]
 
 Restore SciDB data with:
-./scidb_backup.sh restore-opaque <backup directory containing saved data>
--or-
-./scidb_backup.sh restore-binary <backup directory containing saved data>
+./scidb_backup.sh restore-opaque <directory containing saved data> [parallel]
+  -or-
+./scidb_backup.sh restore-binary <directory containing saved data> [parallel]
 
-If the backup directory does not already exist when saving, it will be created.
+Remove backup data and directories (warning!)
+./scidb_backup.sh remove_backup_dirs <directory>
+
+If the backup directory does not already exist, then it will be created.
+
+Set the optional parallel flag to 1 to indicate parallel save/load. Then the
+data directory is assumed to be a subdirectory of each of the SciDB instance
+data directories. Note that parallel save requires ssh access to each SciDB
+node to set up the directories.
+
+If the parallel flag is not specified or not set to 1, then the backup data
+are saved to the specified directory path on the coordinator instance.
+
+The script assumes that SciDB is running on its default port.
 
 The opaque methods are faster and more general, but sometimes the opaque
 save format does not work between SciDB versions. In such cases, use the
 binary methods.
 eof
 
-if test $# -lt 2; then
-  echo "${usage}"
-  exit 1
+NODES="0"
+
+[ $# -lt 2 ] && echo "${usage}" && exit 1
+
+if test $# -gt 2; then
+# Parallel save/load
+  NODES="-1"
+  path="${2}/"
+# In this case, the manifest is only stored on the coordinator.
+  mpath="$(iquery -ocsv -aq "list('instances')" | sed -n 2p | sed -e "s/.*,//" | tr -d "'")/"
+else
+  path="$(readlink -f ${2})/"
+  if test ! -d "${path}"; then
+    mkdir -p "${path}"
+  fi
+  if test ! -d "${path}"; then
+    echo "${usage}"
+    exit 2
+  fi
+  mpath="${path}"
 fi
 
-path=$(readlink -f ${2})
-if test ! -d ${path}; then
-  mkdir -p ${path} 2>/dev/null
-fi
-if test ! -d ${path}; then
-  echo "${usage}"
-  exit 2
+create_dirs ()
+{
+  iquery -ocsv -aq "list('instances')" | sed 1d | while read line;
+  do
+    instance=$(echo $line | sed -e "s/,.*//" | tr -d "'")
+    ipath="$(echo $line | sed -e "s/.*,//" | tr -d "'")/$1"
+    echo "ssh $instance \"mkdir -p ${ipath}\""
+    ssh $instance "mkdir -p ${ipath} && touch ${ipath}/.ok" &
+  done
+  wait
+}
+
+if test "${1}" == "delete_backup_dirs"; then
+  iquery -ocsv -aq "list('instances')" | sed 1d | while read line;
+  do
+    instance=$(echo $line | sed -e "s/,.*//" | tr -d "'")
+    ipath="$(echo $line | sed -e "s/.*,//" | tr -d "'")/$1"
+    echo "ssh $instance \"rm -rf ${ipath}\""
+    ssh $instance "rm -rf ${ipath}" &
+  done
+  wait
+  exit 0
 fi
 
 # Backup array data
 if test "${1}" == "save-opaque"; then
-  iquery -ocsv -aq "list('arrays')" | sed 1d > ${path}/.manifest
-  a=$(cat ${path}/.manifest | cut -d , -f 1 | sed -e "s/'//g")
+  [ "${NODES}" == "-1" ] && create_dirs ${path}
+  iquery -ocsv -aq "list('arrays')" | sed 1d > ${mpath}.manifest
+  a=$(cat ${mpath}.manifest | cut -d , -f 1 | sed -e "s/'//g")
   for x in ${a};do
     echo "Archiving array ${x}"
-    iquery -naq "save(${x}, '${path}/${x}', 0, 'opaque')"
+    iquery -naq "save(${x}, '${path}${x}', ${NODES}, 'opaque')"
   done
   exit 0
 fi
 
 if test "${1}" == "save-binary"; then
-  iquery -ocsv -aq "list('arrays')" | sed 1d > ${path}/.manifest
+  [ "${NODES}" == "-1" ] && create_dirs ${path}
+  iquery -ocsv -aq "list('arrays')" | sed 1d > ${mpath}.manifest
   while read x;
   do
     name=$(echo "${x}" | cut -d , -f 1 | sed -e "s/'//g")
     a=$(echo "${x}" | sed -e "s/.*<//" | sed -e "s/>.*//")
     fmt="($(echo "${a}" | sed -e "s/:/\n/g" | sed -e "s/,/\n/g" | sed -n 'g;n;p' | sed -e "s/DEFAULT.*//" | tr "\n" "," | sed -e "s/,$//"))"
-    query="save(${name}, '${path}/${name}', 0, '${fmt}')"
+    query="save(${name}, '${path}${name}', ${NODES}, '${fmt}')"
     echo "Archiving array ${name}"
     iquery -naq "${query}"
-  done < ${path}/.manifest
+  done < ${mpath}.manifest
   exit 0
 fi
 
-if test ! -f "${path}/.manifest"; then
+if test ! -f "${mpath}.manifest"; then
   echo "Error: can't find .manifest file!"
   exit 4
 fi
@@ -74,10 +122,10 @@ if test "${1}" == "restore-binary"; then
     s=$(echo ${x} | sed -e "s/.*</</" | sed -e "s/'.*//g")
     a=$(echo "${x}" | sed -e "s/.*<//" | sed -e "s/>.*//")
     fmt="($(echo "${a}" | sed -e "s/:/\n/g" | sed -e "s/,/\n/g" | sed -n 'g;n;p' | sed -e "s/DEFAULT.*//" | tr "\n" "," | sed -e "s/,$//"))"
-    query="store(input(${s},'${path}/${name}',0,'${fmt}'),${name})"
+    query="store(input(${s},'${path}${name}',${NODES},'${fmt}'),${name})"
     echo "Restoring array ${name}"
     iquery -naq "${query}"
-  done < ${path}/.manifest
+  done < ${mpath}.manifest
   exit 0
 fi
 
@@ -92,5 +140,5 @@ while read line; do
   s=$(echo ${line} | sed -e "s/.*</</" | sed -e "s/'.*//g")
   echo "Restoring array ${x}"
   iquery -naq "remove(${x})" 2>/dev/null
-  iquery -naq "store(input(${s}, '${path}/${x}', 0, 'opaque'),${x})"
-done < "${path}/.manifest"
+  iquery -naq "store(input(${s}, '${path}${x}', ${NODES}, 'opaque'),${x})"
+done < "${mpath}.manifest"
